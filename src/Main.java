8851202654766ef9b17bac658c78717e363f5689
Ruslan-Far <rfarhetdinov@mail.ru>
2023-03-18 15:18:49 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
 		double c2;
 		double Qw;
 		double L;
+		double L2;
+		double L3;
 		double tau;
 		double h0;
 
@@ -33,18 +35,22 @@ public class Main {
 		}
 		if (Qw % 1 == 0) {
 			L = K * v / Qw + s * Qw / 2 + c1 * v;
+			L2 = K * v / Qw + c1 * v;
 		}
 		else {
 			L = K * v / Qw + s * Qw / 2 + (((int) Qw) * c1 + c2) / tau;
+			L2 = K * v / Qw + (((int) Qw) * c1 + c2) / tau;
 		}
 		h0 = v * td;
 		if (h0 > (int) h0 && h0 <= ((int) h0) + 0.5)
 			h0 = ((int) h0) + 0.5;
 		else
 			h0 = Math.round(h0);
+		L3 = 20 * L + 4 * L2;
 		System.out.println("Оптимальный размер заказа Qw = " + Qw + " л");
 		System.out.println("Общие затраты на управление запасами в единицу времени L = " + L + " руб/дн");
-		System.out.println("Общие затраты на управление запасами в единицу времени L = " + L * 24 + " руб/см");
+		System.out.println("Общие затраты в отсутствие запасов в единицу времени L2 = " + L2 + " руб/дн");
+		System.out.println("Общие затраты за смену L3 = " + L3 + " руб/см");
 		System.out.println("Период поставки tau = " + tau + " дн");
 		System.out.println("Точка заказа h0 = " + h0 + " л");
 	}
