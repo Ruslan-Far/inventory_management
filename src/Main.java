fc672b798c7c3e06496abf5810d60d9e5bae766c
Ruslan-Far <rfarhetdinov@mail.ru>
2023-03-18 13:32:03 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -19,12 +19,6 @@ public class Main {
 		c1 = 10;
 		c2 = 6;
 
-//		v = 0.2 * 83 * 24;
-//		s = 0.12 * 24;
-//		K = 54;
-//		td = (double) 2 / 24 / 24;
-//		c = 10;
-
 		Qw = Math.sqrt(2 * K * v / s);
 		if (Qw > (int) Qw && Qw <= ((int) Qw) + 0.5) {
 			Qw = ((int) Qw) + 0.5;
@@ -32,17 +26,15 @@ public class Main {
 		else {
 			Qw = Math.round(Qw);
 		}
-
-		Qw = 123;
-
 		tau = Qw / v;
+		while (tau > 5) {
+			Qw -= 0.5;
+			tau = Qw / v;
+		}
 		if (Qw % 1 == 0) {
-			System.out.println("-------------------=" + (((int) Qw) * c1) / tau);
-			System.out.println("compare-------------------=" + c1 * v);
 			L = K * v / Qw + s * Qw / 2 + c1 * v;
 		}
 		else {
-			System.out.println("+++++++++++++++++=" + (((int) Qw) * c1 + c2) / tau);
 			L = K * v / Qw + s * Qw / 2 + (((int) Qw) * c1 + c2) / tau;
 		}
 		h0 = v * td;
@@ -52,6 +44,7 @@ public class Main {
 			h0 = Math.round(h0);
 		System.out.println("Оптимальный размер заказа Qw = " + Qw + " л");
 		System.out.println("Общие затраты на управление запасами в единицу времени L = " + L + " руб/дн");
+		System.out.println("Общие затраты на управление запасами в единицу времени L = " + L * 24 + " руб/см");
 		System.out.println("Период поставки tau = " + tau + " дн");
 		System.out.println("Точка заказа h0 = " + h0 + " л");
 	}
